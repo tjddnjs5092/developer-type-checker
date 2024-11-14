5d4050ae04e584dd6054f5ba88138b9690411e1a
tjddnjs5092 <56221694+tjddnjs5092@users.noreply.github.com>
2024-11-14 15:38:20 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // GitHub Pages를 위한 설정
-  basePath: '/developer-type-checker',
-  assetPrefix: '/developer-type-checker/',
-  // 이 부분은 custom domain을 사용하면 필요없습니다
-  // 커스텀 도메인을 사용중이라면 이 두 줄을 제거하세요
+  assetPrefix: './',
+  basePath: '',
 }
 
-export default nextConfig
+module.exports = nextConfig
