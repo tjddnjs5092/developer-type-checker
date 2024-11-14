b77df4aab937dda4098e1fe2f0ab0a8af6e9f8f5
ilgwan <ilgwan.choi@sycns.onmicrosoft.comilgwan.choi@sycns.onmicrosoft.comilgwan.choi@sycns.onmicrosoft.comilgwan.choi@sycns.onmicrosoft.com>
2024-11-14 16:12:45 +0900

    ㅋ

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === "production" ? "/developer-type-checker" : "",
   images: {
