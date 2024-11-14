0cc3ca64aad597b011a09d9d025a48cf8d359d57
tjddnjs5092 <56221694+tjddnjs5092@users.noreply.github.com>
2024-11-14 14:59:07 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/developer-type-checker', // 레포지토리 이름
+}
 
-export default nextConfig;
+export default nextConfig
